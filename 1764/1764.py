b9edf8e9057b64fd5a39e884cc024d83740c5a7d
Lee Jihoon <jihoolee@student.42seoul.kr>
2022-05-16 23:21:22 +0900

    [ADD]: 1764 python timeout fix

--- 1764/1764.py
+++ 1764/1764.py
@@ -6,25 +6,19 @@
 #    By: jihoolee <jihoolee@student.42SEOUL.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/16 18:17:28 by jihoolee          #+#    #+#              #
-#    Updated: 2022/05/16 18:23:47 by jihoolee         ###   ########.fr        #
+#    Updated: 2022/05/16 23:04:19 by jihoolee         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def main():
     not_heard_cnt, not_seen_cnt = map(int, input().split())
-    not_heard_lst = [input() for _ in range(not_heard_cnt)]
-    not_heard_nor_seen_lst = []
+    not_heard_lst = set([input() for _ in range(not_heard_cnt)])
+    not_seen_lst = set([input() for _ in range(not_seen_cnt)])
 
-    for _ in range(not_seen_cnt):
-        name = input()
-        if (name in not_heard_lst):
-            not_heard_nor_seen_lst.append(name)
-
-    not_heard_nor_seen_lst.sort()
+    not_heard_nor_seen_lst = sorted(list(not_heard_lst & not_seen_lst))
     print(len(not_heard_nor_seen_lst))
     for name in not_heard_nor_seen_lst:
         print(name)
-
 
 if (__name__ == '__main__'):
     main()
